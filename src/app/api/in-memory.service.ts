10dc2babea50c668fc58bd1970f2dc70137cc7e7
KadoTomohiro <corner0427@gmail.com>
2018-11-30 01:17:16 +0900

    * Add In Memory API * Add Material

---
+++ src/app/api/in-memory.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService, RequestInfo } from 'angular-in-memory-web-api';
+import { Observable } from 'rxjs';
+import { Todo } from '../shared/model/todo';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InMemoryService implements InMemoryDbService {
+
+  constructor() {
+  }
+
+  createDb(req: RequestInfo): {} | Observable<{}> | Promise<{}> {
+    const todos: Todo[] = [];
+    return {todos};
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryService } from './api/in-memory.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryService)
   ],
   providers: [],
   bootstrap: [AppComponent]
